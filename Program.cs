1838172cc9f6a6db57c3387bb61da64f864b52db
DusanVelimirovic <dusan.velimirovic@yahoo.com>
2024-06-24 22:18:48 +0200

    Adding a Lenght conversions

--- Program.cs
+++ Program.cs
@@ -18,6 +18,22 @@ builder.Services.AddSingleton<ConversionManagerService>(sp =>
     service.RegisterConversion(new KilometersToMeters());
     service.RegisterConversion(new MetersToMiles());
     service.RegisterConversion(new MilesToMeters());
+    service.RegisterConversion(new MetersToCentimeters());
+    service.RegisterConversion(new CentimetersToMeters());
+    service.RegisterConversion(new MillimetersToMeters());
+    service.RegisterConversion(new MetersToMillimeters());
+    service.RegisterConversion(new MilesToKilometers());
+    service.RegisterConversion(new KilometersToMiles());
+    service.RegisterConversion(new MetersToFeet());
+    service.RegisterConversion(new FeetToMeters());
+    service.RegisterConversion(new InchesToCentimeters());
+    service.RegisterConversion(new CentimetersToInches());
+    service.RegisterConversion(new MetersToYards());
+    service.RegisterConversion(new YardsToMeters());
+    service.RegisterConversion(new KilometersToNauticalMiles());
+    service.RegisterConversion(new NauticalMilesToKilometers());
+    service.RegisterConversion(new KilometersToLeagues());
+    service.RegisterConversion(new LeaguesToKilometers());
 
     // Add more conversions here if needed
     return service;
--- Service/LengthConversions.cs
+++ Service/LengthConversions.cs
@@ -67,4 +67,241 @@ namespace Converter_Web_Application.Service
             return value * 1609.34;
         }
     }
+
+
+    public class CentimetersToMeters : IConversion
+    {
+        public int Id => 7;
+
+        public string FromUnit => "centimeters";
+
+        public string ToUnit => "meters";
+
+        public double Convert(double value)
+        {
+            return value / 100;
+        }
+    }
+
+    public class MillimetersToMeters : IConversion
+    {
+        public int Id => 8;
+
+        public string FromUnit => "millimeters";
+
+        public string ToUnit => "meters";
+
+        public double Convert(double value)
+        {
+            return value / 1000;
+        }
+    }
+
+    // Meters to Millimeters: millimeters = meters * 1000
+
+    public class MetersToMillimeters : IConversion
+    {
+        public int Id => 9;
+
+        public string FromUnit => "meters";
+
+        public string ToUnit => "millimeters";
+
+        public double Convert(double value)
+        {
+            return value * 1000;
+        }
+    }
+
+    public class MilesToKilometers : IConversion
+    {
+        public int Id => 10;
+
+        public string FromUnit => "miles";
+
+        public string ToUnit => "kilometers";
+
+        public double Convert(double value)
+        {
+            return value * 1.60934;
+        }
+    }
+
+
+    public class KilometersToMiles : IConversion
+    {
+        public int Id => 11;
+
+        public string FromUnit => "kilometers";
+
+        public string ToUnit => "miles";
+
+        public double Convert(double value)
+        {
+            return value * 0.621371;
+        }
+    }
+
+    // Feet to Meters: meters = feet * 0.3048
+
+    public class FeetToMeters : IConversion
+    {
+        public int Id => 12;
+
+        public string FromUnit => "feet";
+
+        public string ToUnit => "meters";
+
+        public double Convert(double value)
+        {
+            return value * 0.3048;
+        }
+    }
+
+    // Meters to Feet: feet = meters * 3.28084
+    public class MetersToFeet : IConversion
+    {
+        public int Id => 12;
+
+        public string FromUnit => "meters";
+
+        public string ToUnit => "feet";
+
+        public double Convert(double value)
+        {
+            return value * 3.28084;
+        }
+    }
+
+
+    // Centimeters to Inches: inches = centimeters * 0.393701
+
+    public class CentimetersToInches : IConversion
+    {
+        public int Id => 13;
+
+        public string FromUnit => "centimeters";
+
+        public string ToUnit => "inches";
+
+        public double Convert(double value)
+        {
+            return value * 0.393701;
+        }
+    }
+
+    // centimeters = inches * 2.54
+
+    public class InchesToCentimeters : IConversion
+    {
+        public int Id => 14;
+
+        public string FromUnit => "inches";
+
+        public string ToUnit => "centimeters";
+
+        public double Convert(double value)
+        {
+            return value * 2.54;
+        }
+    }
+
+
+    public class YardsToMeters : IConversion
+    {
+        public int Id => 15;
+
+        public string FromUnit => "yards";
+
+        public string ToUnit => "meters";
+
+        public double Convert(double value)
+        {
+            return value * 0.9144;
+        }
+    }
+
+    // Meters to Yards: yards = meters * 1.09361
+
+    public class MetersToYards : IConversion
+    {
+        public int Id => 16;
+
+        public string FromUnit => "meters";
+
+        public string ToUnit => "yards";
+
+        public double Convert(double value)
+        {
+            return value * 1.09361;
+        }
+    }
+
+    // Nautical Miles to Kilometers: kilometers = nautical miles * 1.852
+
+    public class NauticalMilesToKilometers : IConversion
+    {
+        public int Id => 17;
+
+        public string FromUnit => "nautical miles";
+
+        public string ToUnit => "kilometers";
+
+        public double Convert(double value)
+        {
+            return value * 1.852;
+        }
+    }
+
+    // Kilometers to Nautical Miles: nautical miles = kilometers * 0.539957
+
+    public class KilometersToNauticalMiles : IConversion
+    {
+        public int Id => 18;
+
+        public string FromUnit => "kilometers";
+
+        public string ToUnit => "nautical miles";
+
+        public double Convert(double value)
+        {
+            return value * 0.539957;
+        }
+    }
+
+    // Leagues to Kilometers: kilometers = leagues * 4.82803
+
+    public class LeaguesToKilometers : IConversion
+    {
+        public int Id => 19;
+
+        public string FromUnit => "leagues";
+
+        public string ToUnit => "kilometers";
+
+        public double Convert(double value)
+        {
+            return value * 4.82803;
+        }
+    }
+
+    // Kilometers to Leagues: leagues = kilometers * 0.206612
+
+    public class KilometersToLeagues : IConversion
+    {
+        public int Id => 20;
+
+        public string FromUnit => "kilometers";
+
+        public string ToUnit => "leagues";
+
+        public double Convert(double value)
+        {
+            return value * 0.206612;
+        }
+    }
+
+
+
+
 }
