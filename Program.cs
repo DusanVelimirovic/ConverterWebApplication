42f71ce8c9acb315bee801d98702239f3a8e4888
DusanVelimirovic <dusan.velimirovic@yahoo.com>
2024-06-25 12:32:23 +0200

    Adding a more lenght conversions.

--- Program.cs
+++ Program.cs
@@ -15,25 +15,65 @@ builder.Services.AddSingleton<ConversionManagerService>(sp =>
     var service = new ConversionManagerService();
     // Register conversions
     service.RegisterConversion(new MetersToKilometers());
-    service.RegisterConversion(new KilometersToMeters());
+
     service.RegisterConversion(new MetersToMiles());
-    service.RegisterConversion(new MilesToMeters());
+
     service.RegisterConversion(new MetersToCentimeters());
     service.RegisterConversion(new CentimetersToMeters());
-    service.RegisterConversion(new MillimetersToMeters());
+
     service.RegisterConversion(new MetersToMillimeters());
-    service.RegisterConversion(new MilesToKilometers());
-    service.RegisterConversion(new KilometersToMiles());
+
+
     service.RegisterConversion(new MetersToFeet());
     service.RegisterConversion(new FeetToMeters());
     service.RegisterConversion(new InchesToCentimeters());
-    service.RegisterConversion(new CentimetersToInches());
+
     service.RegisterConversion(new MetersToYards());
     service.RegisterConversion(new YardsToMeters());
-    service.RegisterConversion(new KilometersToNauticalMiles());
+
     service.RegisterConversion(new NauticalMilesToKilometers());
-    service.RegisterConversion(new KilometersToLeagues());
+
     service.RegisterConversion(new LeaguesToKilometers());
+
+    // Kilometers
+    service.RegisterConversion(new KilometersToMiles());
+    service.RegisterConversion(new KilometersToMeters());
+    service.RegisterConversion(new KilometersToNauticalMiles());
+    service.RegisterConversion(new KilometersToCentimeters());
+    service.RegisterConversion(new KilometersToMillimeters());
+    service.RegisterConversion(new KilometersToYards());
+    service.RegisterConversion(new KilometersToFeet());
+    service.RegisterConversion(new KilometersToInches());
+    service.RegisterConversion(new KilometersToLeagues());
+
+    // Miles
+    service.RegisterConversion(new MilesToKilometers());
+    service.RegisterConversion(new MilesToMeters());
+    service.RegisterConversion(new MilesToCentimeters());
+    service.RegisterConversion(new MilesToMillimeters());
+    service.RegisterConversion(new MilesToNauticalMiles());
+    service.RegisterConversion(new MilesToYards());
+    service.RegisterConversion(new MilesToFeet());
+    service.RegisterConversion(new MilesToInches());
+
+    // Centimeters
+    service.RegisterConversion(new CentimetersToInches());
+    service.RegisterConversion(new CentimetersToKilometers());
+    service.RegisterConversion(new CentimetersToMiles());
+    service.RegisterConversion(new CentimetersToMillimeters());
+    service.RegisterConversion(new CentimetersToNauticalMiles());
+    service.RegisterConversion(new CentimetersToYards());
+    service.RegisterConversion(new CentimetersToFeet());
+
+    // Millimeters
+    service.RegisterConversion(new MillimetersToMeters());
+    service.RegisterConversion(new MillimetersToKilometers());
+    service.RegisterConversion(new MillimetersToMiles());
+    service.RegisterConversion(new MillimetersToCentimeters());
+    service.RegisterConversion(new MillimetersToYards());
+    service.RegisterConversion(new MillimetersToFeet());
+    service.RegisterConversion(new MillimetersToNauticalMiles());
+
 
     // Add more conversions here if needed
     return service;
--- Service/LengthConversions.cs
+++ Service/LengthConversions.cs
@@ -142,7 +142,6 @@ namespace Converter_Web_Application.Service
         }
     }
 
-    // Feet to Meters: meters = feet * 0.3048
 
     public class FeetToMeters : IConversion
     {
@@ -158,7 +157,6 @@ namespace Converter_Web_Application.Service
         }
     }
 
-    // Meters to Feet: feet = meters * 3.28084
     public class MetersToFeet : IConversion
     {
         public int Id => 12;
@@ -174,7 +172,6 @@ namespace Converter_Web_Application.Service
     }
 
 
-    // Centimeters to Inches: inches = centimeters * 0.393701
 
     public class CentimetersToInches : IConversion
     {
@@ -190,7 +187,6 @@ namespace Converter_Web_Application.Service
         }
     }
 
-    // centimeters = inches * 2.54
 
     public class InchesToCentimeters : IConversion
     {
@@ -221,7 +217,6 @@ namespace Converter_Web_Application.Service
         }
     }
 
-    // Meters to Yards: yards = meters * 1.09361
 
     public class MetersToYards : IConversion
     {
@@ -237,7 +232,6 @@ namespace Converter_Web_Application.Service
         }
     }
 
-    // Nautical Miles to Kilometers: kilometers = nautical miles * 1.852
 
     public class NauticalMilesToKilometers : IConversion
     {
@@ -253,7 +247,6 @@ namespace Converter_Web_Application.Service
         }
     }
 
-    // Kilometers to Nautical Miles: nautical miles = kilometers * 0.539957
 
     public class KilometersToNauticalMiles : IConversion
     {
@@ -269,7 +262,6 @@ namespace Converter_Web_Application.Service
         }
     }
 
-    // Leagues to Kilometers: kilometers = leagues * 4.82803
 
     public class LeaguesToKilometers : IConversion
     {
@@ -285,8 +277,6 @@ namespace Converter_Web_Application.Service
         }
     }
 
-    // Kilometers to Leagues: leagues = kilometers * 0.206612
-
     public class KilometersToLeagues : IConversion
     {
         public int Id => 20;
@@ -300,6 +290,431 @@ namespace Converter_Web_Application.Service
             return value * 0.206612;
         }
     }
+
+    // Kilometers to Centimeters: value × 100000 
+
+    public class KilometersToCentimeters : IConversion
+    {
+        public int Id => 21;
+
+        public string FromUnit => "kilometers";
+
+        public string ToUnit => "centimeters";
+
+        public double Convert(double value)
+        {
+            return value * 100000;
+        }
+    }
+
+    // Kilometers to Millimeters: value * 1000000
+
+    public class KilometersToMillimeters : IConversion
+    {
+        public int Id => 22;
+
+        public string FromUnit => "kilometers";
+
+        public string ToUnit => "millimeters";
+
+        public double Convert(double value)
+        {
+            return value * 1000000;
+        }
+    }
+
+
+    // Kilometers to Yards: value * 1093.61
+
+    public class KilometersToYards : IConversion
+    {
+        public int Id => 23;
+
+        public string FromUnit => "kilometers";
+
+        public string ToUnit => "yards";
+
+        public double Convert(double value)
+        {
+            return value * 1093.61;
+        }
+    }
+
+    // Kilometers to Feet: value * 3280.84
+    public class KilometersToFeet : IConversion
+    {
+        public int Id => 24;
+
+        public string FromUnit => "kilometers";
+
+        public string ToUnit => "feet";
+
+        public double Convert(double value)
+        {
+            return value * 3280.84;
+        }
+    }
+
+
+    // Kilometers to Inches: value * 39370.1
+
+    public class KilometersToInches : IConversion
+    {
+        public int Id => 25;
+
+        public string FromUnit => "kilometers";
+
+        public string ToUnit => "inches";
+
+        public double Convert(double value)
+        {
+            return value * 39370.1;
+        }
+    }
+
+
+
+
+    // Miles to Centimeters: value * 160934
+
+    public class MilesToCentimeters : IConversion
+    {
+        public int Id => 26;
+
+        public string FromUnit => "miles";
+
+        public string ToUnit => "centimeters";
+
+        public double Convert(double value)
+        {
+            return value * 160934;
+        }
+    }
+
+    // Miles to Millimeters: value * 1609340
+
+    public class MilesToMillimeters : IConversion
+    {
+        public int Id => 28;
+
+        public string FromUnit => "miles";
+
+        public string ToUnit => "millimeters";
+        public double Convert(double value)
+        {
+            return value * 1609340;
+        }
+    }
+
+    // Miles to Nautical Miles: value / 1.15078
+
+    public class MilesToNauticalMiles : IConversion
+    {
+        public int Id => 29;
+
+        public string FromUnit => "miles";
+
+        public string ToUnit => "nautica miles";
+        public double Convert(double value)
+        {
+            return value / 1.15078;
+        }
+    }
+
+    // Miles to Yards: value * 1760
+
+    public class MilesToYards : IConversion
+    {
+        public int Id => 30;
+
+        public string FromUnit => "miles";
+
+        public string ToUnit => "yards";
+        public double Convert(double value)
+        {
+            return value * 1760;
+        }
+    }
+
+    // Miles to Feet: value * 5280
+
+    public class MilesToFeet : IConversion
+    {
+        public int Id => 31;
+
+        public string FromUnit => "miles";
+
+        public string ToUnit => "feet";
+        public double Convert(double value)
+        {
+            return value * 5280;
+        }
+    }
+
+    // Miles to Inches: value * 63360
+
+    public class MilesToInches : IConversion
+    {
+        public int Id => 32;
+
+        public string FromUnit => "miles";
+
+        public string ToUnit => "inches";
+        public double Convert(double value)
+        {
+            return value * 63360;
+        }
+    }
+
+
+    // Centimeters to Kilometers: value / 100000
+
+    public class CentimetersToKilometers : IConversion
+    {
+        public int Id => 33;
+
+        public string FromUnit => "centimeters";
+
+        public string ToUnit => "kilometers";
+        public double Convert(double value)
+        {
+            return value / 100000;
+        }
+    }
+
+    // Centimeters to Miles: value / 160934
+
+    public class CentimetersToMiles : IConversion
+    {
+        public int Id => 34;
+
+        public string FromUnit => "centimeters";
+
+        public string ToUnit => "miles";
+        public double Convert(double value)
+        {
+            return value / 160934;
+        }
+    }
+
+    // Centimeters to Millimeters: value * 10
+
+    public class CentimetersToMillimeters : IConversion
+    {
+        public int Id => 35;
+
+        public string FromUnit => "centimeters";
+
+        public string ToUnit => "millimeters";
+        public double Convert(double value)
+        {
+            return value * 10;
+        }
+    }
+
+    // Centimeters to Nautical Miles: value / 185200
+
+    public class CentimetersToNauticalMiles : IConversion
+    {
+        public int Id => 36;
+
+        public string FromUnit => "centimeters";
+
+        public string ToUnit => "nautical miles";
+        public double Convert(double value)
+        {
+            return value / 185200;
+        }
+    }
+
+    // Centimeters to Yards: value / 91.44
+
+    public class CentimetersToYards : IConversion
+    {
+        public int Id => 37;
+
+        public string FromUnit => "centimeters";
+
+        public string ToUnit => "yards";
+        public double Convert(double value)
+        {
+            return value / 91.44;
+        }
+    }
+
+    // Centimeters to Feet: value / 30.48
+    public class CentimetersToFeet : IConversion
+    {
+        public int Id => 38;
+
+        public string FromUnit => "centimeters";
+
+        public string ToUnit => "feet";
+        public double Convert(double value)
+        {
+            return value / 30.48;
+        }
+    }
+
+
+
+
+    // Millimeters to Kilometers: value / 1000000
+
+    public class MillimetersToKilometers : IConversion
+    {
+        public int Id => 39;
+
+        public string FromUnit => "millimeters";
+
+        public string ToUnit => "kilometers";
+        public double Convert(double value)
+        {
+            return value / 1000000;
+        }
+    }
+
+    // Millimeters to Miles: value / 1609340
+
+    public class MillimetersToMiles : IConversion
+    {
+        public int Id => 40;
+
+        public string FromUnit => "millimeters";
+
+        public string ToUnit => "miles";
+        public double Convert(double value)
+        {
+            return value / 1609340;
+        }
+    }
+
+    // Millimeters to Centimeters: value / 10
+
+    public class MillimetersToCentimeters : IConversion
+    {
+        public int Id => 41;
+
+        public string FromUnit => "millimeters";
+
+        public string ToUnit => "centimeters";
+        public double Convert(double value)
+        {
+            return value / 10;
+        }
+    }
+
+    // Millimeters to Nautical Miles: value / 1852000
+
+    public class MillimetersToNauticalMiles : IConversion
+    {
+        public int Id => 42;
+
+        public string FromUnit => "millimeters";
+
+        public string ToUnit => "nautical miles";
+        public double Convert(double value)
+        {
+            return value / 1852000;
+        }
+    }
+
+    // Millimeters to Yards: value / 914.4
+
+    public class MillimetersToYards : IConversion
+    {
+        public int Id => 43;
+
+        public string FromUnit => "millimeters";
+
+        public string ToUnit => "yards";
+        public double Convert(double value)
+        {
+            return value / 914.4;
+        }
+    }
+
+    // Millimeters to Feet: value / 304.8
+
+    public class MillimetersToFeet : IConversion
+    {
+        public int Id => 44;
+
+        public string FromUnit => "millimeters";
+
+        public string ToUnit => "feet";
+        public double Convert(double value)
+        {
+            return value / 304.8;
+        }
+    }
+
+
+
+
+    // Nautical Miles to Meters: value * 1852
+
+    // Nautical Miles to Centimeters: value * 185200
+
+    // Nautical Miles to Millimeters: value * 1852000
+
+    // Nautical Miles to Yards: value * 2025.37
+
+    // Nautical Miles to Feet: value * 6076.12
+
+    // Nautical Miles to Inches: value * 72913.4
+
+    // Yards to Kilometers: value / 1093.61
+
+    // Yards to Miles: value / 1760
+
+    // Yards to Centimeters: value * 91.44
+
+    // Yards to Millimeters: value * 914.4
+
+    // Yards to Nautical Miles: value / 2025.37
+
+    // Yards to Inches: value * 36
+
+    // Feet to Kilometers: value / 3280.84
+
+    // Feet to Miles: value / 5280
+
+    // Feet to Centimeters: value * 30.48
+
+    // Feet to Millimeters: value * 304.8
+
+    // Feet to Nautical Miles: value / 6076.12
+
+    // Feet to Yards: value / 3
+
+    // Inches to Meters: value / 39.3701
+
+    // Inches to Kilometers: value / 39370.1
+
+    // Inches to Miles: value / 63360
+
+    // Inches to Millimeters: value * 25.4
+
+    // Inches to Nautical Miles: value / 72913.4
+
+    // Inches to Yards: value / 36
+
+    // Inches to Feet: value / 12
+
+    // Leagues to Meters: value * 5556
+
+    // Leagues to Centimeters: value * 555600
+
+    // Leagues to Millimeters: value * 5556000
+
+    // Leagues to Nautical Miles: value * 3
+
+    // Leagues to Yards: value * 5280
+
+    // Leagues to Feet: value * 15840
+
+    // Leagues to Inches: value * 190080
 
 
 
