cfa01df368dd8a7ce4805f3b51ab1aee5310466c
DusanVelimirovic <dusan.velimirovic@yahoo.com>
2024-06-29 16:35:09 +0200

    Developed speed conversion. Manual testing.

--- Program.cs
+++ Program.cs
@@ -480,8 +480,37 @@ builder.Services.AddSingleton<ConversionManagerService>(sp =>
     service.RegisterConversion(new TerabytesToGigabytes());
     service.RegisterConversion(new TerabytesToTerabits());
 
+    // Register Speed conversions
 
+    // Meters per Second
+    service.RegisterConversion(new MetersPerSecondToKilometersPerHour());
+    service.RegisterConversion(new MetersPerSecondToMilesPerHour());
+    service.RegisterConversion(new MetersPerSecondToFeetPerSecond());
+    service.RegisterConversion(new MetersPerSecondToKnots());
 
+    // Kilometers Per Hour
+    service.RegisterConversion(new KilometersPerHourToMetersPerSecond());
+    service.RegisterConversion(new KilometersPerHourToMilesPerHour());
+    service.RegisterConversion(new KilometersPerHourToFeetPerSecond());
+    service.RegisterConversion(new KilometersPerHourToKnots());
+
+    // Miles per Hour
+    service.RegisterConversion(new MilesPerHourToMetersPerSecond());
+    service.RegisterConversion(new MilesPerHourToKilometersPerHour());
+    service.RegisterConversion(new MilesPerHourToFeetPerSecond());
+    service.RegisterConversion(new MilesPerHourToKnots());
+
+    // Feet Per Second
+    service.RegisterConversion(new FeetPerSecondToMetersPerSecond());
+    service.RegisterConversion(new FeetPerSecondToKilometersPerHour());
+    service.RegisterConversion(new FeetPerSecondToMilesPerHour());
+    service.RegisterConversion(new FeetPerSecondToKnots());
+
+    // Knots To Meters
+    service.RegisterConversion(new KnotsToMetersPerSecond());
+    service.RegisterConversion(new KnotsToKilometersPerHour());
+    service.RegisterConversion(new KnotsToMilesPerHour());
+    service.RegisterConversion(new KnotsToFeetPerSecond());
 
 
     // Add more conversions here if needed
