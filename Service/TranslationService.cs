527bf1659d2c17b0abd58ad36e6d798ef01553aa
DusanVelimirovic <dusan.velimirovic@yahoo.com>
2024-07-12 16:29:21 +0200

    Small changes.

--- Service/TranslationService.cs
+++ Service/TranslationService.cs
@@ -16,7 +16,7 @@ namespace Converter_Web_Application.Service
 
         public async Task InitializeAsync()
         {
-            var translationFiles = new[] { "en.json", "fr.json" };
+            var translationFiles = new[] { "en.json", "fr.json", "it.json", "ru.json", "sr.json" };
             foreach (var file in translationFiles)
             {
                 var culture = Path.GetFileNameWithoutExtension(file);
